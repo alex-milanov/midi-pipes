a9fa34d78704f2866cec54a451c5adc74a97dc24
Alex Milanov <alex@i4web.biz>
2020-04-27 00:44:10 +0300

    refactored gtk signal handling, common callback for value change, toggle channel input based on control midi note type

---
+++ some-refs.md
@@ -0,0 +1,3 @@
+https://stackoverflow.com/questions/11808819/how-do-i-pass-multiple-variables-as-data-with-gtk-signals
+https://stackoverflow.com/questions/2229498/passing-by-reference-in-c
+https://github.com/michaelwu/alsa-lib/blob/afb2fbd0e554e42e51325c3197a176ea96a74203/include/seqmid.h
--- src/main.c
+++ src/main.c
@@ -119,6 +119,11 @@ void * midi_thread(void * context_ptr)
     switch (ev->type) {
       case SND_SEQ_EVENT_NOTEON:
         sprintf(str_midi_msg, "%s %d %d %d",
+        // sprintf(str_midi_msg, "i %d o %d r %d %d | %s %d %d %d",
+          // ev->source.port,
+          // ev->dest.port,
+          // out_port,
+          // out_ports[out_port],
           note_type_text(ev->type),
           ev->data.note.channel,
           ev->data.note.note,
@@ -155,12 +160,30 @@ void * midi_thread(void * context_ptr)
 void midi_init(void)
 {
   // int ret;
-  seq = midi_init_client(in_ports, 1, out_ports, 2);
+  seq = midi_init_client(in_ports, 2, out_ports, 2);
 
   // ret = pthread_create(&g_alsa_midi_tid, NULL, midi_thread, NULL);
   pthread_create(&g_alsa_midi_tid, NULL, midi_thread, NULL);
 }
 
+void on_combobox_changed_to_int(GtkComboBox *comboBox, int *d) {
+  *d = (int)gtk_combo_box_get_active(comboBox);
+}
+
+void on_spinbutton_value_changed_to_int(GtkSpinButton *spinButton, int *d) {
+  *d = (int)gtk_spin_button_get_value(spinButton);
+}
+
+void on_type_change_toggle_widget(GtkComboBox *comboBox, GtkWidget *widget) {
+  switch ((int)gtk_combo_box_get_active(comboBox)) {
+    case 0:
+      gtk_widget_show(widget);
+      break;
+    case 1:
+      gtk_widget_hide(widget);
+      break;
+  }
+}
 
 int main(int argc, char *argv[])
 {
@@ -183,27 +206,47 @@ int main(int argc, char *argv[])
 
     // select prev fields
     g_lbl_prev = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_prev"));
+    // channel
     g_num_prev_channel = GTK_WIDGET(gtk_builder_get_object(builder, "num_prev_channel"));
     gtk_spin_button_set_value(GTK_SPIN_BUTTON(g_num_prev_channel), prev_pref.channel);
+    g_signal_connect(g_num_prev_channel, "value-changed",
+      G_CALLBACK(on_spinbutton_value_changed_to_int), &prev_pref.channel);
     g_dd_prev_msg_type = GTK_WIDGET(gtk_builder_get_object(builder, "dd_prev_msg_type"));
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(g_dd_prev_msg_type), note_type_map[0].text);
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(g_dd_prev_msg_type), note_type_map[1].text);
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(g_dd_prev_msg_type), note_type_map[2].text);
     gtk_combo_box_set_active(GTK_COMBO_BOX(g_dd_prev_msg_type), prev_pref.msg_type);
+    g_signal_connect(g_dd_prev_msg_type, "changed",
+      G_CALLBACK(on_combobox_changed_to_int), &prev_pref.msg_type);
+    g_signal_connect(g_dd_prev_msg_type, "changed",
+      G_CALLBACK(on_type_change_toggle_widget), g_num_prev_channel);
+    // msg note
     g_num_prev_msg_note = GTK_WIDGET(gtk_builder_get_object(builder, "num_prev_msg_note"));
     gtk_spin_button_set_value(GTK_SPIN_BUTTON(g_num_prev_msg_note), (double)prev_pref.msg_note);
+    g_signal_connect(g_num_prev_msg_note, "value-changed",
+      G_CALLBACK(on_spinbutton_value_changed_to_int), &prev_pref.msg_note);
 
     // select next fields
     g_lbl_next = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_next"));
+    // channel
     g_num_next_channel = GTK_WIDGET(gtk_builder_get_object(builder, "num_next_channel"));
     gtk_spin_button_set_value(GTK_SPIN_BUTTON(g_num_next_channel), next_pref.channel);
+    g_signal_connect(g_num_next_channel, "value-changed",
+      G_CALLBACK(on_spinbutton_value_changed_to_int), &next_pref.channel);
     g_dd_next_msg_type = GTK_WIDGET(gtk_builder_get_object(builder, "dd_next_msg_type"));
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(g_dd_next_msg_type), note_type_map[0].text);
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(g_dd_next_msg_type), note_type_map[1].text);
     gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(g_dd_next_msg_type), note_type_map[2].text);
     gtk_combo_box_set_active(GTK_COMBO_BOX(g_dd_next_msg_type), next_pref.msg_type);
+    g_signal_connect(g_dd_next_msg_type, "changed",
+      G_CALLBACK(on_combobox_changed_to_int), &next_pref.msg_type);
+    g_signal_connect(g_dd_next_msg_type, "changed",
+      G_CALLBACK(on_type_change_toggle_widget), g_num_next_channel);
+    // msg note
     g_num_next_msg_note = GTK_WIDGET(gtk_builder_get_object(builder, "num_next_msg_note"));
     gtk_spin_button_set_value(GTK_SPIN_BUTTON(g_num_next_msg_note), (double)next_pref.msg_note);
+    g_signal_connect(g_num_next_msg_note, "value-changed",
+      G_CALLBACK(on_spinbutton_value_changed_to_int), &next_pref.msg_note);
 
     g_object_unref(builder);
 
@@ -221,28 +264,6 @@ void on_window_main_show()
   midi_init();
 }
 
-// select prev
-void on_dd_prev_msg_type_changed(GtkComboBox *comboBox, gpointer d) {
-  prev_pref.msg_type = (int)gtk_combo_box_get_active(comboBox);
-}
-void on_num_prev_channel_value_changed(GtkSpinButton *spinButton,  gpointer d) {
-  prev_pref.channel = (int)gtk_spin_button_get_value(spinButton);
-}
-void on_num_prev_msg_note_value_changed(GtkSpinButton *spinButton,  gpointer d) {
-  prev_pref.msg_note = (int)gtk_spin_button_get_value(spinButton);
-}
-
-// select next
-void on_dd_next_msg_type_changed(GtkComboBox *comboBox, gpointer d) {
-  next_pref.msg_type = (int)gtk_combo_box_get_active(comboBox);
-}
-void on_num_next_channel_value_changed(GtkSpinButton *spinButton,  gpointer d) {
-  next_pref.channel = (int)gtk_spin_button_get_value(spinButton);
-}
-void on_num_next_msg_note_value_changed(GtkSpinButton *spinButton,  gpointer d) {
-  next_pref.msg_note = (int)gtk_spin_button_get_value(spinButton);
-  // printf("spin-button2-vlaue: %f\n", gtk_spin_button_get_value(spinButton));
-}
 
 void on_radio_output_0_toggled (GtkToggleButton *togglebutton, GtkLabel *label)
 {
